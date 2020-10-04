f20db142a44148fb43e0750ea8930bafdf570ff0
홍준호 <brenden0730@gmail.com>
2020-10-04 16:52:42 +0900

    WRITE REST API Sample Code

--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"io/ioutil"
+	"github.com/gin-gonic/gin"
+)
+
+// HomePage : GET, "/"
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello World!",
+	})
+}
+
+// PostHomePage : GET, "/"
+func PostHomePage(c *gin.Context) {
+	body := c.Request.Body
+	value, err := ioutil.ReadAll(body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	c.JSON(200, gin.H{
+		"message": string(value),
+	})
+}
+
+// QueryStrings : GET, "/query"
+func QueryStrings(c *gin.Context) {
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age" : age,
+	})
+}
+
+// PathParameters : GET, "/path"
+func PathParameters(c *gin.Context) {
+	name := c.Param("name")
+	age := c.Param("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age" : age,
+	})
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.GET("/", HomePage)
+	r.POST("/", PostHomePage)
+	r.GET("/query", QueryStrings) // /query?name=brenden&age=20
+	r.GET("/path/:name/:age", PathParameters) // /path/brenden/20
+	r.Run()
 }
